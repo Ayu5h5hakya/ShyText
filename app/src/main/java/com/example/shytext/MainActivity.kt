6009b3c82ebde0d0dbe2fef945b1a727f0256c86
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-06-11 11:44:30 +0545

    add spacer for canvas

--- app/src/main/java/com/example/shytext/MainActivity.kt
+++ app/src/main/java/com/example/shytext/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.shytext.ui.theme.ShyTextTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,5 +36,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ShyText(modifier: Modifier = Modifier) {
+    Spacer(
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .drawBehind {
 
+            }
+    )
 }
