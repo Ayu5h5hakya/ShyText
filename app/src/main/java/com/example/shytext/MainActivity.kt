3dfea347876af1feadff014f0bb0f647bc36e16e
Ayu5h5hakya <qwerty.ayush5@gmail.com>
2024-06-11 09:19:42 +0545

    add text measurer

--- app/src/main/java/com/example/shytext/MainActivity.kt
+++ app/src/main/java/com/example/shytext/MainActivity.kt
@@ -14,18 +14,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.shytext.ui.theme.ShyTextTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sampletext =
+            "longTextSample asdasdas asdasd asdasdas dasd asd asd asdasd asd as adasd asd redacted ada sd asdasd asd  asdas d asda sd as  asdasdasd asdas asd asdasda asdasd asdsadasdasdasda sdfsdfs dsdfs dfsd fsdfdd sdfsdfs dfsdfergdbddcbff tbfgnjkyuk ykyukhjk,uiluifhf g edrgdfgdrtgr ttrthgfbhfghrtyfhbfg rt hrthfg ffff   fghfghrtyrthfgn s"
+
         enableEdgeToEdge()
         setContent {
             ShyTextTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ShyText(
+                        text = sampletext,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -35,13 +46,21 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ShyText(modifier: Modifier = Modifier) {
+fun ShyText(text: String, modifier: Modifier = Modifier) {
+    val textMeasurer = rememberTextMeasurer()
     Spacer(
         modifier = Modifier
             .height(100.dp)
             .fillMaxWidth()
             .drawBehind {
-
+                val measuredText =
+                    textMeasurer.measure(
+                        AnnotatedString(text),
+                        overflow = TextOverflow.Ellipsis,
+                        constraints = Constraints.fixed(size.width.toInt(), size.height.toInt()),
+                        style = TextStyle(fontSize = 18.sp)
+                    )
+                drawText(measuredText)
             }
     )
 }
